10aec28299aae8c6102aa0510e0748e3685ddcf5
JheyluisReyes <141370176+JheyluisReyes@users.noreply.github.com>
2023-08-06 09:54:42 -0400

    Opens with Python

---
+++ speechrecognition.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+mic = sr.Microphone()
+
+with mic as source:
+    audio = recognizer.listen(source)
+    text = recognizer.recognize_google(audio, language = 'en-US')
+print('You said: ')
+input()
